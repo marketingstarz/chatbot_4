e631b1eda3a4dcfb98e01a5c7e8a91573a7c9be6
marketingstarz github <git@marketingstarz.com>
2018-01-29 04:06:05 -0800

    added handing of postbacks
    added handing of postbacks

--- index.js
+++ index.js
@@ -142,6 +142,19 @@ function handleMessage(sender_psid, received_message) {
 // handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
 
+	let response;
+
+	// get the payload for the postback
+	let payload = received_postback.payload;
+
+	// set the response based on the postback payload
+	if (payload === 'yes') {
+		response = { "text": "Thanks!" }
+	} else if (payload === 'no') {
+		response = { "text": "Oops, try sending another image." }
+	}
+	// send the message to acknowledge the postback
+	callSendAPI(sender_psid, response);
 }
 
 // sends response messages via the Send API
